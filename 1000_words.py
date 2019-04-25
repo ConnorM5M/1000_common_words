06f1eee9f1f9b771fd6af3171c955f87681658d8
Connor McCloskey <connor@connors-air.lan>
2019-04-25 11:51:34 -0500

    fixed edge cases and optimized runtime

--- 1000_words.py
+++ 1000_words.py
@@ -1,46 +1,42 @@
 import requests
-import xlwt
 from xlwt import Workbook
 from bs4 import BeautifulSoup
 
 wb = Workbook()
-sheet1 = wb.add_sheet('Sheet 1') 
+common_words = wb.add_sheet('1000 common words')
 
 
-
-def check_language(language):
+def check_language(lang):
 	language_page = requests.get('https://1000mostcommonwords.com/').text
 	language_soup = BeautifulSoup(language_page, 'lxml')
 
-	languages = []
-
 	for i in language_soup.select('.entry-content p span a'):
-		if language.lower() == i.text.lower():
+		if lang.lower() == i.text.lower():
 			return True
 		else:
 			continue
 	return False
 
 
-language = input("What language would you like to see?")
-if (check_language(language)):
+language = input("What language would you like to see? ")
+
+
+if check_language(language):
 	link = 'https://1000mostcommonwords.com/1000-most-common-' + language.lower() + '-words/'
-	#'https://1000mostcommonwords.com/1000-most-common-german-words/'
 	page = requests.get(link).text
 	soup = BeautifulSoup(page, 'lxml')
 
-	words = []
 	count = 0
 
 	for title in soup.select('.entry-content table tbody tr'):
-		print(type(title.text.splitlines()[1]))
-		sheet1.write(count, 0, title.text.splitlines()[3] + ",")
-		sheet1.write(count, 1, title.text.splitlines()[2])
-		count += 1
-		words.append(title.text)
+		if count == 0:
+			common_words.write(count, 0, title.text.splitlines()[2])
+			common_words.write(count, 1, 'English')
+		else:
+			common_words.write(count, 0, title.text.splitlines()[2] + ',')
+			common_words.write(count, 1, title.text.splitlines()[3])
 
-	#for i in range(len(words)):
-	#	print(words[i])
+		count += 1
 else:
 	print("language not found")
 
